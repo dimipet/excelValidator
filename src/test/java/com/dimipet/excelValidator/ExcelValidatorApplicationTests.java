21ef9dcd057edda5076df807db0662a724f57bea
dimipet <dimi.petridis@gmail.com>
2020-06-04 14:20:17 +0300

    initial commit - spring initializr

---
+++ src/test/java/com/dimipet/excelValidator/ExcelValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimipet.excelValidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
